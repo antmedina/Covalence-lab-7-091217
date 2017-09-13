b5954241e3f6b971ffa4c3613c5053a2d482be59
antmedina <ant.n.medina@gmail.com>
2017-09-12 20:03:50 -0400

    initial process of gen dice and random value works now

--- JS-Lab-7-091217.js
+++ JS-Lab-7-091217.js
@@ -1,1 +1,39 @@
-$(document).ready()
+//set an array for new objects to populate//
+var objArray = [];
+// create object class //
+var Dice = function (value) {
+    this.value = this.roll();
+    this.div = document.createElement("div");
+    this.div.innerText = this.value;
+    this.div.className = "dice";
+//append created dice elements to container
+    document.getElementById('container').appendChild(this.div);
+//event to "roll" the value of each dice
+    this.div.addEventListener("click", function () {
+        this.value = this.roll();
+        this.div.innerText = this.value;
+    }.bind(this));
+}
+
+//object class function to randomize dice roll//
+    Dice.prototype.roll = function () {
+        return Math.floor((Math.random() * 6) + 1);
+    };
+//this generates a new dice and pushes it to the obj array//
+    document.getElementById('genDie').addEventListener('click',function(){
+            var d = new Dice();
+            objArray.push(d);
+    })    
+
+// }.bind(this);
+
+
+// function addDice() {
+//     var newDice = document.createElement("div");
+//     newDice.className = "dice"
+//     newDice.innerText = this.value;
+//     document.body.appendChild(newDice);
+//     container.appendChild(newDice);
+// }
+
+
