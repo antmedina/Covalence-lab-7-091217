191c745fef3bcda162e42815111ed03f35a42a20
antmedina <ant.n.medina@gmail.com>
2017-09-12 21:32:33 -0400

    Add sum works and even did multiplication, plus click to change number event. dblclick does not sum  up right

--- JS-Lab-7-091217.js
+++ JS-Lab-7-091217.js
@@ -13,28 +13,56 @@ var Dice = function (value) {
         this.value = this.roll();
         this.div.innerText = this.value;
     }.bind(this));
- //remove a dice from array   
-    this.div.addEventListener("dblclick", function (){
-        this.div.remove();
-        var id = objArray.indexOf(this);
-    }.bind(this));
+//remove a dice from array   
+    // this.div.addEventListener("dblclick", function () {
+    //     this.div.remove();
+    //     var id = objArray.indexOf(this);
+    // }.bind(this));
 }
 
 //object class function to randomize dice roll//
-    Dice.prototype.roll = function () {
-        return Math.floor((Math.random() * 6) + 1);
-    };
+Dice.prototype.roll = function () {
+    return Math.floor((Math.random() * 6) + 1);
+};
 //this generates a new dice and pushes it to the obj array//
-    document.getElementById('genDie').addEventListener('click',function(){
-            var d = new Dice();
-            objArray.push(d);
-    });    
-
-document.getElementById("rollDice").addEventListener("click",function (){
-    for (var i = 0; i<objArray.length; i++){
-    objArray[i].value = objArray[i].roll();
-    objArray[i].div.innerText = objArray[i].value;
+document.getElementById('genDie').addEventListener('click', function () {
+    var d = new Dice();
+    objArray.push(d);
+});
+//created rollDice function
+document.getElementById("rollDice").addEventListener("click", function () {
+//runs the length of the array
+    for (var i = 0; i < objArray.length; i++) {
+//create new value
+        objArray[i].value = objArray[i].roll();
+//append new value
+        objArray[i].div.innerText = objArray[i].value;
     }
-})
+});
+//create sumDice function
+document.getElementById("sumDice").addEventListener("click", function () {
+ //set base total of 0 and create variable
+    var total = 0;
+ //set function to run the length of the array
+    for (var i = 0; i < objArray.length; i++) {
+//adds total + the extent of the array's values
+        total += objArray[i].value;
+    }
+//make an window pop up on click
+    alert("The sum of all dice add up to " + total);
+});
+//extra pracite with multiplication
+document.getElementById("multiplyDice").addEventListener("click", function () {
+//set base total of 1 because it's multiplication
+    var total = 1;
+//set function to run the length of the array
+    for (var i = 0; i < objArray.length; i++) {
+//multiplies total + the extent of the array's values
+        total *= objArray[i].value;
+    }
+//make an window pop up on click
+    alert("The sum of all dice add up to " + total);
+});
+
 
 
